f3a6062779e8dd225ae9d66225d07cde9ebf65b0
rockguitarist1 <wzsoccerplyr@aim.com>
2019-02-12 19:24:48 -0500

    Update channel id and embed for live stream

--- src/app/shared/services/youtube.service.ts
+++ src/app/shared/services/youtube.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 const ytKey = 'AIzaSyCwAnY1pbey1YaBVu4pyxsuG6ouCuW_RQc';
-const channelID = 'UCooQ1REzRs_uU_AJuEsrXEw';
+const channelID = 'UC3TyCy83iqLJ16pLYWT3ZGg';
 const liveURL =
   `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&eventType=live&maxResults=1&type=video&key=${ytKey}`;
 
